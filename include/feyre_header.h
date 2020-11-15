8c006a4b11e9bc95a7b817c8a8664322d542d8b0
Rowan Rathod <stem@rowanrathod.com>
2020-11-15 23:45:47 +1300

    Initial Files
    Started the project

---
+++ include/feyre_header.h
@@ -0,0 +1,21 @@
+//
+// Created by rowan on 15/11/20.
+//
+
+#ifndef FEYRE_FEYRE_HEADER_H
+#define FEYRE_FEYRE_HEADER_H
+
+#include <Arduino.h>
+#include <SPI.h>
+#include <U8g2lib.h>
+#include <LoRa.h>
+
+#define APP_NAME "Feyre - LoRa Communicator"
+
+#define DEFAULT_SERIAL_SPEED 9600
+#define LOCAL_COMPONENT_SERIAL_SPEED 115200
+#define DEFAULT_FREQ 915E6
+
+#define NEW_LINE "\n";
+
+#endif //FEYRE_FEYRE_HEADER_H
---
+++ src/main.cpp
@@ -0,0 +1,78 @@
+/***
+ * Title: Feyre LoRa Bond
+ * Author: Rowan Rathod
+ * Last Updated: 15 November 2020
+ *
+ */
+
+#include "feyre_header.h"
+
+String buffer = "";
+int packetSize = -1;
+
+void setup() {
+    pinMode(LED_BUILTIN, OUTPUT);
+
+    // Initialises External Serial Port
+    Serial.begin(DEFAULT_SERIAL_SPEED);
+
+    // Turns on the Status Light if the Serial Port failed to initialise
+    if(!Serial){
+        digitalWrite(LED_BUILTIN, HIGH);
+        while(!Serial);
+    }
+    // Ensures the Status Light is Off upon success
+    digitalWrite(LED_BUILTIN, LOW);
+
+
+    // Initialises Inter-Component Serial Port
+    Serial3.begin(LOCAL_COMPONENT_SERIAL_SPEED);
+    // Blinks Status Light if Serial Port failed to initialise
+    bool lightOn = true;
+    while(!Serial3) {
+        digitalWrite(LED_BUILTIN, lightOn ? HIGH : LOW);
+        lightOn = !lightOn;
+        delay(500);
+    }
+    // Ensures the Status Light is Off upon success
+    digitalWrite(LED_BUILTIN, LOW);
+
+    // Turns off Status LED of Serial Port initialised successfully
+    digitalWrite(LED_BUILTIN, LOW);
+
+    // Prints out the app name
+    Serial.println(APP_NAME);
+
+    // Attempts to Start the LoRa Module
+    Serial.println("Starting LoRa Module...");
+    while(!LoRa.begin(DEFAULT_FREQ)) {
+        Serial.println("Starting LoRa failed! Trying again in");
+        for(int i=5; i>0; i--){
+            Serial.print(String(i));
+            Serial.println("...");
+            delay(1000);
+        }
+    }
+    Serial.println("Success!");
+}
+
+void loop() {
+    buffer = "";
+    packetSize = LoRa.parsePacket();
+    if (packetSize) {
+        // received a packet
+        Serial.print("Received packet '");
+
+        // read packet
+        while (LoRa.available()) {
+            buffer += (char)LoRa.read();
+        }
+
+        // Sends received buffer to the Rhys Module
+        Serial3.print(buffer);
+
+        // print RSSI of packet
+        Serial3.print("' with RSSI ");
+        Serial3.println(LoRa.packetRssi());
+    }
+}
